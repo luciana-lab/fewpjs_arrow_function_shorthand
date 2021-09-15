6ce264cf18c22830b82c222777fa5db2b674ba89
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-15 13:19:48 -0400

    commit tree 64725d8fe30547e5169323758261c5a7b4cfd0cc

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1) has a functional expression called divide
+// 2) divide divides 2000 by 100
+const divide = function () {
+    return 2000 / 100;
+};
+
+// 1) has an arrow function called square
+// 2) square arrow function multiplies a number times itself
+const square = number => number * number;
+
+// 1) has an arrow function called add
+// 2) add arrow function takes two parameters and adds them together
+const add = (parameter1, parameter2) => parameter1 + parameter2;
